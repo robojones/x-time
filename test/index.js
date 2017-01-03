d21056aa89cc58238a64ea9df19ba39298cef805
robojones <jonathan.scholz-weil@t-online.de>
2017-01-03 13:22:05 +0100

    add test, set node version to >=6.0.0

---
+++ test/index.js
@@ -0,0 +1,39 @@
+const xTime = require('./../index')
+
+const assert = require('assert')
+
+describe('xTime', function () {
+
+    it('should return a promise', function () {
+
+        let r = xTime(0)
+
+        assert(r instanceof Promise)
+    })
+
+    it('should resolve with the given value', function () {
+
+        let arg = "test-1001"
+
+        return xTime(10, arg).then(v => {
+            assert.strictEqual(v, arg)
+        })
+    })
+    
+    it('should resolve after the given time (circa 1000ms)', function () {
+
+        let r = xTime(1000)
+
+        let t0 = process.hrtime()
+
+        return r.then(() => {
+            let t = process.hrtime(t0)
+
+            let duration = t[0] *1000 + Math.round(t[1] /1000000)
+
+            if((!duration > 900 && duration < 1100)){
+                cb(new Error(`expected duration to be circa 1000ms - instead got ${duration}`))
+            }
+        })
+    })
+})
