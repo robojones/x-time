52bf58ff8de8ed313562b4a9d953d2619799c5b9
robojones <jonathan.scholz-weil@t-online.de>
2017-08-19 00:32:26 +0200

    add JSDoc definition

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * Returns a promise that resolves after the time to the value
+ * @param {number} time - Number of milliseconds 
+ * @param {*} [value] - Resolve value
+ * @returns {Promise.<*>}
+ */
 function xTime(time, value) {
   return new Promise(resolve => setTimeout(resolve, time, value))
 }
